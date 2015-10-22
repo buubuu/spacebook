f982dcb3a40176aba441006f676108ee80e3ee3a
Mrs Omotosho <busayomiomotosho10@gmail.com>
2015-10-22 13:41:15 -0700

    completed!

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	
+	$('#redirectApply').hide();
+
+	function amountforRent(){
+		rental_hours = $('#freehours').val();
+		rental_amount = $('#rental_amount').val();
+		var standard_amount = 20000;
+		if (rental_amount != 0){
+			rental_sum = rental_hours * rental_amount;
+			return rental_sum;	
+		}
+		else{
+			rental_amount = standard_amount;
+			rental_sum = rental_hours * rental_amount;
+			return rental_sum;
+		}
+
+	}
+
+	$('#calculate').click(function(){
+		var rental = amountforRent();
+
+		$('.section__text').fadeOut('fast');
+		$('div #new_result').replaceWith('You could make up to &nbsp <b> &#8358;' +rental_sum + ' in a day!! </b>');
+		$('#function_button').fadeIn('slow');
+		$('#function_button #calculate span').fadeIn('slow').text('Apply Now');
+		
+	})
+
+})
